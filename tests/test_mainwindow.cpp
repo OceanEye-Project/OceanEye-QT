878d8a9ce2cf53b82262841bc0e05b2570138fa5
Dale Foerster <dalefoerster@uvic.ca>
2024-08-22 21:57:04 -0700

    testFilterDeadVideo functional

--- tests/test_mainwindow.cpp
+++ tests/test_mainwindow.cpp
@@ -1,23 +1,62 @@
-// Executable command: ./build/OceanEyeTest.app/Contents/MacOS/OceanEyeMainWindowTest
+// Executable command: ./build/OceanEyeMainWindowTest.app/Contents/MacOS/OceanEyeMainWindowTest
 #include <QtTest/QtTest>
 #include "../src/core/mainwindow.h"
+#include "../src/core/welcomewindow.h"
 
 class MainWindowTest : public QObject
 {
     Q_OBJECT
 
+private:
+    std::shared_ptr<Project> project;
+    MainWindow* mainWindow;
+
 private slots:
-    void initTestCase();
-    void mainWindowTest();
+    void init();
+    void cleanup();
+    void testConstructor();
+    void testLoadModel();
+    void testFilterDeadVideo();
 };
 
-// Define the test functions
-void MainWindowTest::initTestCase() {
-    // Initialization code here
+void MainWindowTest::init() {
+    WelcomeWindow welcomeWindow(project);
+    welcomeWindow.loadProjectFromPath("test_project");
+    mainWindow = new MainWindow(project);
 }
 
-void MainWindowTest::mainWindowTest(){
-    // code here
+void MainWindowTest::cleanup() {
+    delete mainWindow;
+    mainWindow = nullptr;
+}
+
+void MainWindowTest::testConstructor() {
+    QVERIFY(mainWindow->ui != nullptr);
+    QVERIFY(mainWindow->currentProject == project);
+}
+
+void MainWindowTest::testLoadModel()
+{
+}
+
+void MainWindowTest::testFilterDeadVideo() {
+    mainWindow->loadModel("../models/large_model.onnx");
+
+    mainWindow->addMedia({"OceanEyeVid.mov"});
+    
+    mainWindow->currentProject->settings.setValue("Automatically Filter Dead Video", true);
+    
+    QSignalSpy spy(mainWindow, &MainWindow::doneSlicing);
+    spy.wait(8000);
+
+    // Check the media size
+    qDebug() << "MEDIA SIZE = " << mainWindow->currentProject->media.size();
+    QVERIFY(mainWindow->currentProject->media.size() == 13);
+}
+
+
+void testLoadModel(){
+    
 }
 
 // Register the test class
