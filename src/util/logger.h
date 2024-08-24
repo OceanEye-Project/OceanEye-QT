fc9ead65de988317fd6c9539b6108a44054d273e
Konrad Jasman <kjasman@uvic.ca>
2024-08-24 18:06:18 -0400

    Added header code for custom logger object

--- src/util/logger.h
+++ src/util/logger.h
@@ -1,47 +1,57 @@
-// ======================== Logger ========================
 /**
+ * =================================== Logger Class ===================================
+ * Custom message log handler. To be initialized ONCE in main.cpp, as QT logging is handled 
+ * globally. Cleanup should be called at the end of main.cpp to ensure resources are cleaned up.
+ * 
+ * This header implementation provides overrides for the following:
+ * 
+ * - qInfo()
+ * - qDebug()
+ * - qWarning()
+ * - qFatal()
+ * - qCritical()
+ * 
+ * Refer to https://doc.qt.io/qt-6/qtlogging.html#QtMsgType-enum for custom logging with Qt.
+ * 
+ * Note that when using with standard strings, the QString::fromStdString() function must be used
+ * to convert std::string objects into QString objects. To use this functionality, QString should 
+ * included.
+ * 
+ * Using logging in project files within the global scope requires inclusion of only qDebug
+ * 
+ * ====================================================================================
  * 
  */
+#ifndef LOGGER_H  // Logger_H
+#define LOGGER_H
 
+// Include necessary Qt headers
 #include <QMessageLogger>
 #include <QFile>
 #include <QTextStream>
 #include <QDateTime>
+#include <cstdlib>
 
+// Namespace to avoid naming conflicts with other logging utilities
+namespace Logger {
 
-// Set up logger
-QFile logFile("log.txt");
-QTextStream ls;
+    // Logger class declaration
+    class Logger {
+    public:
+        // Public static methods for initializing and cleaning up the logger
+        static void init(const QString& filename = "yolov8_log.txt");
+        static void cleanup();
 
-// Message handler
-void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-{
-    QByteArray localMsg = msg.toLocal8Bit();
-    const char *file = context.file ? context.file : "";
-    const char *function = context.function ? context.function : "";
-    QString txtMessage;
+    private:
+        // Private static method for handling log messages
+        static void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
-    switch (type) {
-    case QtDebugMsg:
-        txtMessage = QString("Debug: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(file).arg(context.line).arg(function);
-        break;
-    case QtInfoMsg:
-        txtMessage = QString("Info: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(file).arg(context.line).arg(function);
-        break;
-    case QtWarningMsg:
-        txtMessage = QString("Warning: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(file).arg(context.line).arg(function);
-        break;
-    case QtCriticalMsg:
-        txtMessage = QString("Critical: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(file).arg(context.line).arg(function);
-        break;
-    case QtFatalMsg:
-        txtMessage = QString("Fatal: %1 (%2:%3, %4)").arg(localMsg.constData()).arg(file).arg(context.line).arg(function);
-        abort();
-    }
+        // Private static members for file handling and state management
+        static QFile logFile;
+        static QTextStream ls;
+        static bool isInitialized;
+    };
 
-    QDateTime currentTime = QDateTime::currentDateTime();
-    QString formattedMessage = currentTime.toString("yyyy-MM-dd hh:mm:ss.zzz ") + txtMessage + "\n";
-    ls << formattedMessage;
-    ls.flush();
-}
-// End of logging class. 
+    } // namespace Logger
+
+#endif // LOGGER_H
